6c2694f9a1b1470ada26055bfad480b960b1b5dd
jonhatanh <jonhatan.higuera@gmail.com>
2023-12-12 23:08:06 -0600

    Refactor if condition

--- recursion.js
+++ recursion.js
@@ -18,4 +18,40 @@ function fibsRec(num, secuence = []) {
     let actualNum = num1[num1.length-1] + num2[num2.length-1];
     return num1.concat(actualNum);
 }
-console.log(fibsRec(8));
+// console.log(fibsRec(8));
+
+function mergeSort(arr) {
+  if(arr.length === 1) {
+    return arr;
+  }
+
+  let leftMerge = mergeSort(arr.slice(0, (arr.length/2)))
+  let rightMerge = mergeSort(arr.slice((arr.length/2)))
+  let sortedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < leftMerge.length && rightIndex < rightMerge.length) {
+    if(leftMerge[leftIndex] > rightMerge[rightIndex]) {
+      sortedArray.push(rightMerge[rightIndex])
+      rightIndex++;
+    } else {
+      sortedArray.push(leftMerge[leftIndex])
+      leftIndex++;
+    } 
+  }
+
+  sortedArray = leftIndex === leftMerge.length 
+    ? sortedArray.concat(rightMerge.slice(rightIndex))
+    : sortedArray.concat(leftMerge.slice(leftIndex))
+  return sortedArray;
+  // if(leftIndex === leftMerge.length) {
+  //   sortedArray = sortedArray.concat(rightMerge.slice(rightIndex))
+  //   return(sortedArray);
+  // // } else if (rightIndex === rightMerge.length) {
+  // } else {
+  //   sortedArray = sortedArray.concat(leftMerge.slice(leftIndex))
+  //   return(sortedArray);
+  // }
+}
+
+console.log(mergeSort([6,5,12,10,9,1]));
